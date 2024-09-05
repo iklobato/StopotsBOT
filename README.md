aced9e63a83c4f07cbd4a936a5d5c59eb0a9f938
henrique <iklobato1@gmail.com>
2024-09-04 23:16:43 -0300

    updates

--- README.md
+++ README.md
@@ -1,5 +1,82 @@
-# StopotsBOT
+# StopotsBot
 
-- Got dictionary.json from Lucas8x (https://github.com/Lucas8x/stopots-bot/blob/master/dictionary.json)
+## Overview
+StopotsBot is an automated bot designed to play the game "Stopots" by automatically filling in the answers based on pre-defined categories and letters. This bot interacts with the Stopots website using Playwright, filling out form fields and retrieving scores from the game interface. The answers are generated based on a JSON dictionary of predefined letter-category answers.
 
-[![Demonstration](https://i.vimeocdn.com/video/877131222.webp?mw=700&mh=393)](https://player.vimeo.com/video/406621151)
+## How It Works
+The bot operates through the following steps:
+1. **Launching a Browser:** Uses Playwright to control a Chromium-based browser and navigate to the Stopots game page.
+2. **Automated Interaction:**
+   - Fills in the username and joins the game.
+   - Listens for the game's current letter and category, then fills in the answers based on predefined mappings in `dictionary.json`.
+   - Automatically clicks necessary buttons to interact with the game (e.g., "OK", "Estou Pronto").
+3. **Scoring and Updates:**
+   - Periodically retrieves and logs the user scores by parsing the game interface.
+4. **Dynamic Answer Generation:** For each letter and category, it retrieves the appropriate answer from the dictionary file and fills it in the game form fields.
+
+## Usage
+To run the bot, ensure you have Python installed, along with the required dependencies.
+
+### Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/iklobato/StopotsBOT.git
+   cd stopotsbot
+    ```
+2. Install the dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+3. Run the bot:
+    ```bash
+    python stopotsbot.py --headless --username <username>
+    ```
+    - `--headless`: Run the browser in headless mode (without a GUI).
+    - `--username`: Your username to use in the game.
+
+### Dictionary File Structure
+The bot uses a JSON file (dictionary.json) to generate the answers. The file structure looks like this:
+```json
+{
+    "A": {
+        "Animais": ["Anta", "Antílope", "Avestruz"],
+        "Cidades": ["Aracaju", "Aparecida", "Araguaína"]
+    },
+    "B": {
+        "Animais": ["Baleia", "Borboleta", "Búfalo"],
+        "Cidades": ["Barueri", "Belo Horizonte", "Blumenau"]
+    }
+}
+```
+
+### Logging and Output
+The bot logs its actions, such as the letter being used, the category, and the generated answers. It also logs the score updates for all players in the game.
+```bash
+2024-09-04 23:11:46,630 - INFO - Current letter: I
+2024-09-04 23:11:46,656 - INFO - [I 1] Nome Masculino: Ivo
+2024-09-04 23:11:46,767 - INFO - +---------------+----------+
+| user          |   points |
++===============+==========+
+| ik/3          |      110 |
+| xxxxxxx       |       50 |
+| xxxxxxxxxxxxx |       40 |
+| xxxxxx        |       20 |
+| xxxx          |        0 |
+| xxxxxxxxxx    |        0 |
+| xxxxx         |        0 |
+| xxxxxxxxxx    |        0 |
+| xxxxxxxxx     |        0 |
+| xxxx          |        0 |
++---------------+----------+
+2024-09-04 23:11:46,800 - INFO - [I 2] Sabor De Pizza: Italiana
+2024-09-04 23:11:46,917 - INFO - [I 3] Desenho Animado: Invasor Zim
+2024-09-04 23:11:47,073 - INFO - [I 4] Verbo: Imitar
+2024-09-04 23:11:47,192 - INFO - [I 5] Pais: Italia
+2024-09-04 23:11:47,348 - INFO - [I 6] Time Esportivo: Irlanda
+2024-09-04 23:11:47,468 - INFO - [I 7] Doce: I-Naosei
+2024-09-04 23:11:47,586 - INFO - [I 8] Profissao: Instrumentista
+2024-09-04 23:11:47,705 - INFO - [I 9] Remedio: Ibuprofeno
+2024-09-04 23:11:47,825 - INFO - [I 10] Palavra Em Ingles: Investigation
+2024-09-04 23:11:47,974 - INFO - [I 11] Pch: Intestino Grosso
+2024-09-04 23:11:48,085 - INFO - [I 12] Meio De Transporte: Iate
+```
---
+++ app.py
@@ -0,0 +1,180 @@
+
+from argparse import ArgumentParser
+
+from tabulate import tabulate
+from unidecode import unidecode
+import asyncio
+import json
+import logging
+from random import choice, random
+
+from playwright.async_api import async_playwright
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+
+LABELS_MAP = {
+    'CEPCidade, Estado ou Pais': 'cep',
+    'GentilicoAdjetivo patrio (Ex: Brasileiro, Mineiro, Curitibano...)': 'gentilico',
+    'FLVFruta, Legume ou Verdura': 'flv',
+    'JLRJornal, Livro ou Revista': 'jlr',
+    'PCHParte do Corpo Humano': 'pch',
+    'PDAPersonagem de Desenho Animado': 'pda',
+    'MSEMinha Sogra E...': 'mse',
+}
+
+STOPOTS_URL = "https://stopots.com/system/"
+letters_answers_file = open("dictionary.json")
+letters_answers_json = json.load(letters_answers_file)
+letters_answers_file.close()
+
+
+async def check_and_press_ok_button(page):
+    button_xpath = '/html/body/div[1]/div[1]/div[2]/div/div/div/div[2]/button/strong'
+    b = await page.query_selector(f'xpath={button_xpath}')
+    if not b:
+        return
+    button_text = await page.text_content(f'xpath={button_xpath}')
+    is_disable = await page.evaluate(f'el => el.disabled', f'xpath={button_xpath}')
+    if not is_disable and button_text and button_text.strip().lower() in ['ok']:
+        await page.click(f'xpath={button_xpath}')
+
+
+async def get_word_answer(chosen_letter: str, category: str):
+    category_parsed = unidecode(category)
+    letter_info = letters_answers_json.get(chosen_letter, {})
+    answer = letter_info.get(category_parsed)
+    if not answer:
+        return f"{chosen_letter}-NaoSei"
+    answer = choice(answer)
+    return answer
+
+
+def compare_two_dicts(dict1, dict2):
+    """
+    Compare two dictionaries, return False if they are different, True if they are the same.
+    :param dict1:
+    :param dict2:
+    :return:
+    """
+    if any([dict1 is None, dict2 is None]):
+        return False
+    for key, value in dict1.items():
+        if key not in dict2:
+            return False
+        if value != dict2[key]:
+            return False
+    return True
+
+
+async def print_score(page, current_points=None) -> dict:
+    users_points = {}
+    users = await page.locator('ul#users li').all()
+    if not users or len(users) == 0:
+        return current_points
+
+    for user in users:
+        username = await user.locator('.nick').text_content()
+        try:
+            points_text = await user.locator('span').text_content()
+            points = int(points_text.replace(' pts', '').strip())
+            users_points[unidecode(username.strip())] = points
+        except Exception as e:
+            logging.warning(f"Could not retrieve points for user {username}. Error: {e}")
+            return {}
+
+    if not compare_two_dicts(users_points, current_points):
+        logging.info(
+            tabulate(
+                sorted(users_points.items(), key=lambda x: x[1], reverse=True),
+                headers=['user', 'points'],
+                tablefmt='outline'
+            )
+        )
+        return users_points
+    return current_points
+
+
+async def click_ready_if_exists(page):
+    ready_button_xpath = '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[2]/div/button/strong'
+    button_text = await page.text_content(f'xpath={ready_button_xpath}')
+    is_disable = await page.evaluate(f'el => el.disabled', f'xpath={ready_button_xpath}')
+    if not is_disable and button_text and button_text.strip().lower() in ['estou pronto', 'avaliar']:
+        await page.click(f'xpath={ready_button_xpath}')
+        await asyncio.sleep(1)
+
+
+async def run(_playwright, args):
+    logging.info("Launching browser...")
+    browser = await _playwright.chromium.launch(headless=args.headless)
+    context = await browser.new_context(
+        locale="pt-BR",
+    )
+    page = await context.new_page()
+    page.set_default_timeout(90_000)
+
+    logging.info(f"Navigating to {STOPOTS_URL}")
+    await page.goto(STOPOTS_URL)
+
+    username_input_xpath = '/html/body/div[1]/div[1]/div[1]/div/div[2]/div[2]/div[1]/div[2]/input'
+    header_button_xpath = '/html/body/header/div[1]/div[2]/div[1]/form/button/strong'
+    submit_button_xpath = '/html/body/div[1]/div[1]/div[1]/div/div[2]/div[2]/div[2]/button[1]'
+
+    if await page.query_selector(f'xpath={username_input_xpath}') is None:
+        await page.click(f'xpath={header_button_xpath}')
+
+    await page.fill(f'xpath={username_input_xpath}', args.username)
+    await page.click(f'xpath={submit_button_xpath}')
+
+    last_letter = None
+    current_users_points = None
+    while True:
+        await check_and_press_ok_button(page)  # anti ban
+        await click_ready_if_exists(page)  # ready button
+        current_users_points = await print_score(page, current_users_points)
+
+        letter_xpath = '/html/body/div[1]/div[1]/div[1]/div/div/div[1]/div[2]/div[2]/div/ul/li[1]/span'
+        letter = await page.text_content(f'xpath={letter_xpath}')
+        if letter == last_letter:
+            await asyncio.sleep(1)
+            continue
+
+        last_letter = letter
+        logging.info(f"Current letter: {letter.upper()}")
+
+        for i in range(1, 13):
+            label_xpath = f'/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[2]/div/div[1]/label[{i}]/span'
+            input_xpath = f'/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[2]/div/div[1]/label[{i}]/input'
+
+            if await page.query_selector(f'xpath={label_xpath}') is None:
+                break
+
+            input_text = await page.input_value(f'xpath={input_xpath}')  # Await input_value()
+            if input_text and input_text != "":
+                continue
+
+            label_text = await page.text_content(f'xpath={label_xpath}')  # Await text_content()
+            label_text = unidecode(label_text)
+            label_text = LABELS_MAP.get(label_text, label_text)
+            answer_text = await get_word_answer(letter.lower(), label_text.lower())
+            await page.fill(f'xpath={input_xpath}', answer_text)
+            logging.info(f"[{letter.upper()} {i}] {label_text.title()}: {answer_text.title()}")
+
+            current_users_points = await print_score(page, current_users_points)
+
+
+async def main():
+    parameters = ArgumentParser()
+    parameters.add_argument('--headless', action='store_true', help='Run in headless mode')
+    parameters.add_argument('--username', type=str, help='Username to use in the game')
+    args = parameters.parse_args()
+    async with async_playwright() as p:
+        await run(p, args)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
